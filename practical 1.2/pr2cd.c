9d5700a47aedf54d1b360f5de1d1e166fd033454
Suchi <vadgamasuchia@gmail.com>
2024-12-22 10:40:19 +0530

    Add files via upload

---
+++ practical 1.2/pr2cd.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int validatePattern(const char *str) {
+    int len = strlen(str);
+
+    if (str == NULL || len < 3) {
+        return 0;
+    }
+
+    if (len == 3) {
+        return (str[0] == 'a' && str[1] == 'b' && str[2] == 'b') ? 1 : 0;
+    }
+
+    for (int i = 0; i < len - 2; i++) {
+        if (str[i] != 'a') {
+            return 0;
+        }
+    }
+
+    if (str[len - 2] == 'b' && str[len - 1] == 'b') {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main() {
+    char input[100];
+
+    printf("Enter a string: ");
+    scanf("%s", input);
+
+    if (validatePattern(input)) {
+        printf("Valid String\n");
+    } else {
+        printf("Invalid String\n");
+    }
+
+    return 0;
+}
+
